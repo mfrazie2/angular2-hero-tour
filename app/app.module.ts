ecbdaea6c89cd95b89973bc869c49a0ef27e87c8
Mike Frazier <michael.s.frazier@gmail.com>
2016-09-23 12:16:26 -0600

    Update app module with new components

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+
+
+import { HeroService } from './hero.service';
+
+import { routing } from './app.routing';
 
 @NgModule({
-	imports: [ BrowserModule, FormsModule ],
-	declarations: [ AppComponent, HeroDetailComponent ],
-	bootstrap: [ AppComponent ]
+	imports: [ BrowserModule, FormsModule, routing ],
+	declarations: [
+		AppComponent,
+		HeroesComponent,
+		HeroDetailComponent,
+		DashboardComponent
+	],
+	bootstrap: [ AppComponent ],
+	providers: [ HeroService ]
 })
 
 export class AppModule {}

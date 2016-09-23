6990abe65b6231af26f407a636970d8dfdcac082
Mike Frazier <michael.s.frazier@gmail.com>
2016-09-23 12:17:44 -0600

    Removed app component information

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -5,7 +5,6 @@ import { HeroService } from './hero.service';
 @Component({
 	selector: 'my-heroes',
 	template: `
-						<h1>{{ title }}</h1>
 						<h2>My Heroes</h2>
 						<ul class="heroes">
 							<li *ngFor="let hero of heroes"
@@ -65,10 +64,9 @@ import { HeroService } from './hero.service';
       border-radius: 4px 0 0 4px;
     }
 		`],
-		providers: [ HeroService ],
+		providers: [],
 })
 export class HeroesComponent implements OnInit {
-	title = "Tour of Heroes";
 	selectedHero: Hero;
 	heroes: Hero[];
 
